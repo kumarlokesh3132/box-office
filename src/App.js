775a70a772aa6ffdc4a04a853659e38d3805b160
lokeshkumar3132 <kumarlokesh3132@gmail.com>
2021-05-26 17:14:04 +0530

    added router to the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Switch, Route} from 'react-router-dom'
 
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Switch>
+<Route exact path="/">
+This is the home page
+
+</Route>
+<Route exact path="/starred">
+This is the starred page
+
+</Route>
+<Route>
+  This is 404 page
+</Route>
+
+    </Switch>
   );
 }
 
