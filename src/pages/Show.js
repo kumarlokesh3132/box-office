ed5c22fa4486b64ba401700d012bb0479fb3b554
lokeshkumar3132 <kumarlokesh3132@gmail.com>
2021-05-31 14:33:31 +0530

    addad loading and error messages

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,14 +7,43 @@ const Show = () => {
   const { id } = useParams();
 
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   
   useEffect(() =>{
+
+    let isMounted = true;
     getAPI(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results =>{
-      setShow(results);
+
+    
+      if(isMounted){
+        setShow(results);
+      setIsLoading(false);
+      }
+      
+    }
+    ).catch(err => {
+      if(isMounted){
+          
+      setError(err.meesage);
+      setIsLoading(false);
+      }
     });
+
+    return () => {
+      isMounted = false;
+    }
 
   }, [id])
   console.log('show', show)
+
+  if(isLoading){
+    return<div>Data is Loading</div>
+  }
+
+  if(error){
+    return<div>Error occured: {error}</div>
+  }
 
     return <div>This is show page</div>
 }
